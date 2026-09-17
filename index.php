c7ae395468075c528f6bb608fd0faf1dd2c33e76
125052997-hash <125052997@upq.edu.mx>
2026-09-17 15:34:29 -0600

    se hizo un html de un hola

---
+++ index.php
@@ -0,0 +1,28 @@
+<!-- <?php
+echo "Heoll"
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: #b65db6;;
+        }
+
+    </style>
+</head>
+<body>
+    <h1> hola </h1>
+    <h2> hola </h2>
+ <h3> hola </h3>
+    <h4> hola </h4>
+ <h5> hola </h5>
+    <h6> hola </h6>
+
+
+</body>
+</html>
