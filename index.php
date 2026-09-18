1265ba7d840b21f62c42cd7f77d23ad39e62cb9e
125052997-hash <125052997@upq.edu.mx>
2026-09-18 17:28:53 -0600

    se agregaron dos textos nuevos, y se uso padding, margin y border

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!-- <?php
-echo "Heoll"
+echo"hello"
 ?> -->
 
 <!DOCTYPE html>
@@ -10,19 +10,52 @@ echo "Heoll"
     <title>Document</title>
     <style>
         h1{
-            color: #b65db6;;
+            color: #00d16c;;
+        }
+        .seccion{
+            background-color: #d10000;
+            color: white;
+            padding: 20px 40px;
+            margin: 20px;
+            border: 10px hidden black;
         }
 
+       .seccion2{
+            background-color: #0096d1;
+            color: white;
+padding-top: 20px;
+padding-right: 40px; 
+padding-bottom: 20px; 
+padding-left: 40px;  
+
+margin-top: 20px;
+margin-right: 40px; 
+margin-bottom: 20px; 
+margin-left: 40px;  
+   } 
+        
     </style>
 </head>
 <body>
-    <h1> hola </h1>
-    <h2> hola </h2>
- <h3> hola </h3>
-    <h4> hola </h4>
- <h5> hola </h5>
-    <h6> hola </h6>
+    <h1>hola</h1>
+    <h2>hola</h2>
+    <h3>hola</h3>
+    <h4>hola</h4>
+    <h5>hola</h5>
+    <h6>hola</h6>
+    <div class = "seccion">
+        <h1>Titulo</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam iste delectus quis natus, 
+        aspernatur eius maiores harum odio debitis quisquam consectetur dignissimos, perspiciatis 
+        saepe quam doloribus non necessitatibus dicta pariatur?</p>
 
 
+    </div>
+    <div class = "seccion2">
+        <h1>Titulo</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam iste delectus quis natus, 
+        aspernatur eius maiores harum odio debitis quisquam consectetur dignissimos, perspiciatis 
+        saepe quam doloribus non necessitatibus dicta pariatur?</p>
+    </div>
 </body>
 </html>
